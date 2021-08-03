dfc33798d11c067788eef4255c3e3c29f23e4642
ShimmyD <dongshimiao@gmail.com>
2021-08-03 14:27:34 -0600

    add notebook and web app

---
+++ README.md
@@ -0,0 +1,48 @@
+# Bertelsmann/Arvato Project
+
+### Table of content
+1. Introduction
+2. Description
+3. Web App
+4. Installation
+5. Acknowledgement
+
+## Introduction
+
+In this project, we will analyze demographics data for customers of a mail-order sales company in Germany, comparing it against demographics information for the general population.
+We'll use unsupervised learning techniques to perform customer segmentation, identifying the parts of the population that best describe the core customer base of the company. 
+Then, we'll apply what we've learned on a third dataset with demographics information for targets of a marketing campaign for the company, and use a model to predict which 
+individuals are most likely to convert into becoming customers for the company. The data used has been provided by our partners at Bertelsmann Arvato Analytics, and represents 
+a real-life data science task.
+
+## Description
+### Data
+* DIAS Attributes Values 2017.xlsx and DIAS Information Levels Attributes 2017.xlsx are metadata file for all datasets used in this project
+* Udacity_MAILOUT_052018_TRAIN.csv and Udacity_MAILOUT_052018_TEST.csv are training and testing sets for building supervised ML. Test set is the testing set on Kaggle for ML score
+* variables known.csv, unknowns_0.csv, unknowns_-1.csv and unknowns_9.csv are metadata file summary files, used for data cleasing
+
+### Project structure
+1. Data explorartory
+2. Customer Segmentation
+  1.1 Data pre-processing
+  1.2 Dimensionality reduction
+  1.3 Clustering
+3. Build supervised ML
+4. Kaggle submission 
+
+## Web Application
+A web application is built to predict company's potential customer with our trained supervised ML. Python library flask is used for back end work and bootstrap library
+is used for front end work. The trained model takes 24 individual demographics inputs as model attributes, as these 24 demographics info are the most weighted features evaluated by
+our model. Then outputs whether or not this person is a potential customer for the company.
+
+## Installation
+1. Create virtual environment with command 'py -m venv enviro_name', activate virtual environment  'enviro_name\scripts\activate.bat'
+2. Download requirements.txt and install all libraries required to run this web application on local server with command 'pip install -r requirements.txt'
+3. Download data and templates folders and app.py.
+4. Run the following command in the directory where app.py is in to run your web app.`python app.py`
+5. Then go to  http://127.0.0.1:3001/
+
+## Acknowledgement 
+Thanks to Udacity and Bertelsmann/Arvato for providing this fun and challenging project!
+
+
---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.graph_objs import Bar
+import uuid
+import os
+import json
+import plotly
+import joblib
+
+app = Flask(__name__)
+attributes=["AGER_TYP" ,"CJT_GESAMTTYP" ,"D19_BANKEN_DIREKT" ,"D19_BANKEN_OFFLINE_DATUM " ,"D19_LEBENSMITTEL" ,"D19_LOTTO" ,"D19_SOZIALES", "D19_TECHNIK",
+"D19_TIERARTIKEL" ,"FINANZ_HAUSBAUER" ,"GEBURTSJAHR","GFK_URLAUBERTYP" ,"GFK_URLAUBERTYP_3","KBA05_ANHANG ","KBA05_DIESEL" ,"KBA05_MAXHERST" 
+,"KBA05_SEG2","KBA05_VORB0","KBA13_KRSHERST_FORD_OPEL" ,"KBA13_SEG_GELAENDEWAGEN" ,"KBA13_SEG_VAN" ,"PRAEGENDE_JUGENDJAHRE", "WOHNLAGE"]
+
+# with open('data/mailout_train.pkl', 'rb') as fp:
+#     train = pickle.load(fp)
+
+train_null=pd.read_csv('data/train_missing.csv')
+response=pd.read_csv('data/train_response.csv')
+
+with open('data/model_ada_less.pkl', 'rb') as fp:
+    ada = pickle.load(fp)
+
+# null_col_df=train.columns[train.isnull().sum()!=0]
+# null_percent_df=(train[null_col_df].isnull().sum()/len(train))[:20]
+# order=null_percent_df.sort_values(ascending=False).index
+
+@app.route("/")
+def hello_world():
+    # query = request.args.get('query', '') 
+    response_count = response.RESPONSE.value_counts().values
+    response_names = [0,1]
+
+    x_null=train_null.iloc[:,0].values
+    y_null=train_null.iloc[:,1].values
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=response_names,
+                    y=response_count
+                )
+            ],
+
+            'layout': {
+                'title': 'Customer Response Distribution',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Response"
+                }
+            }
+        }
+        ,{
+            'data': [
+                Bar(
+                    x=x_null,
+                    y=y_null
+                )
+            ],
+
+            'layout': {
+                'title': 'Trianset missing value percentage_top 20 variables',
+                'yaxis': {
+                    'title': "Missing percent"
+                },
+                'xaxis': {
+                    'title': "varialbes"
+                }
+            }
+        }
+    ]
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+@app.route('/go')
+def go():
+    # save user input in query
+    attribute_values=arr=np.zeros((1,23))
+    for i in range(len(attributes)):
+        # print(attributes[i])
+        # print(request.args.get(attributes[i], ''))
+        attribute_values[0,i]=float(request.args.get(attributes[i], '') )
+
+    # use model to predict classification for query
+    prob=ada.predict_proba(attribute_values)[0][1]
+    
+    if prob>0.36:
+        classification_label='A Potential Customer'
+    else:
+        classification_label='Not a Potential Customer'
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        classification_label=classification_label
+    )
+def main():
+    app.run(host='127.0.0.1', port=3001, debug=True)
+
+if __name__ == '__main__':
+    main()
