8030b761024e42b8f89ace2a56644fc8a28cafe2
ShimmyD <dongshimiao@gmail.com>
2021-08-03 16:19:41 -0600

    chore: updating html in web app

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ attributes=["AGER_TYP" ,"CJT_GESAMTTYP" ,"D19_BANKEN_DIREKT" ,"D19_BANKEN_OFFLIN
 "D19_TIERARTIKEL" ,"FINANZ_HAUSBAUER" ,"GEBURTSJAHR","GFK_URLAUBERTYP" ,"GFK_URLAUBERTYP_3","KBA05_ANHANG ","KBA05_DIESEL" ,"KBA05_MAXHERST" 
 ,"KBA05_SEG2","KBA05_VORB0","KBA13_KRSHERST_FORD_OPEL" ,"KBA13_SEG_GELAENDEWAGEN" ,"KBA13_SEG_VAN" ,"PRAEGENDE_JUGENDJAHRE", "WOHNLAGE"]
 
-# with open('data/mailout_train.pkl', 'rb') as fp:
-#     train = pickle.load(fp)
 
 train_null=pd.read_csv('data/train_missing.csv')
 response=pd.read_csv('data/train_response.csv')
@@ -25,13 +23,8 @@ response=pd.read_csv('data/train_response.csv')
 with open('data/model_ada_less.pkl', 'rb') as fp:
     ada = pickle.load(fp)
 
-# null_col_df=train.columns[train.isnull().sum()!=0]
-# null_percent_df=(train[null_col_df].isnull().sum()/len(train))[:20]
-# order=null_percent_df.sort_values(ascending=False).index
-
 @app.route("/")
 def hello_world():
-    # query = request.args.get('query', '') 
     response_count = response.RESPONSE.value_counts().values
     response_names = [0,1]
 
@@ -84,11 +77,12 @@ def hello_world():
 @app.route('/go')
 def go():
     # save user input in query
-    attribute_values=arr=np.zeros((1,23))
+    attribute_values=np.zeros((1,23))
     for i in range(len(attributes)):
         # print(attributes[i])
         # print(request.args.get(attributes[i], ''))
         attribute_values[0,i]=float(request.args.get(attributes[i], '') )
+    # input_dict = dict(zip(attributes, attribute_values.tolist()[0]))
 
     # use model to predict classification for query
     prob=ada.predict_proba(attribute_values)[0][1]
@@ -101,7 +95,7 @@ def go():
     # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
-        classification_label=classification_label
+        classification_label=classification_label,attributes=attributes,values=attribute_values.tolist()[0]
     )
 def main():
     app.run(host='127.0.0.1', port=3001, debug=True)
